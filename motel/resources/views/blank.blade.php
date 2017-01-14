39987a76be6dc762442a64c924fde849170a5205
Marco Hernandez <marco.hernandez@globant.com>
2017-01-14 12:38:27 -0500

    Added auth and blank template

---
+++ motel/resources/views/blank.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" type="text/css" href="{{ URL::Asset('lib/bootstrap/dist/css/bootstrap.min.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ URL::Asset('lib/bootstrap/dist/css/bootstrap-theme.min.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ URL::Asset('css/styles.css') }}" />
+
+        <title>Motel La Hacienda</title>
+    </head>
+    <body>
+        <h1>Motel La Hacienda</h1>
+
+        <script type="text/javascript" src="{{URL::Asset('lib/jquery/dist/jquery.slim.min.js')}}"></script>
+        <script type="text/javascript" src="{{URL::Asset('lib/bootstrap/dist/js/bootstrap.min.js')}}"></script>
+    </body>
+</html>
